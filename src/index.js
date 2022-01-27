65cb01f49715b88da1b7d9a13ea6e2676158c425
6uilder <mihajlovsv@mail.ru>
2022-01-27 21:58:08 +0300

    fix: made my own dict

--- src/index.js
+++ src/index.js
@@ -39,10 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     let phrase = [];
-    let myExpr =  Object.assign({}, expr);
-    myExpr.split('');
-
-    return phrase;
+    let newDict = {};
+    let goodDict = {};
+    for (let [key, value] of Object.entries(MORSE_TABLE)) {
+			newDict[value] = key.replace(/[.]/g, '10').replace(/[-]/g, '11').padStart(10, '0');
+    }
+    newDict[' '] =  '**********';
+    
+    for (let [key, value] of Object.entries(newDict)) {
+			goodDict[value] = key;
+    }
+    
+    let str = '';
+    for (let i = 0; i < expr.length; i+=10) {
+    	phrase.push(expr.slice(i, i+10))
+    }
+    
+		for (let i of phrase) {
+    	str += goodDict[i]
+    }
+        
+    return str;
 }
 
 module.exports = {
